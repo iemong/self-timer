7894a8b452f299b73ec391d071cb521c345b1a34
iemong <kiyoshidainagon@gmail.com>
2021-08-22 23:48:17 +0900

    画像を保存するところまでは作った

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,98 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+    import Button from '@smui/button'
+    import download from 'downloadjs'
+
+    let videoRef:(HTMLVideoElement | null)
+    let canvasForSave:(HTMLCanvasElement | null)
+    let videoSize: { width: number, height: number} = { width: 0, height: 0}
+    let isCapturing = false
+
+    const displayMediaOptions:MediaStreamConstraints = {
+        video: true,
+        audio: false
+    }
+
+    const startCapture = async () => {
+        try {
+            if(!videoRef) return
+            const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+            videoRef.srcObject = stream
+
+            isCapturing = true
+            const currentTrack = stream.getVideoTracks()[0]
+            currentTrack.onended = () => {
+                stopCapture()
+            };
+            const { width, height} = currentTrack.getSettings()
+            videoSize.width = width
+            videoSize.height = height
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    const stopCapture = () => {
+        if ('getTracks' in videoRef.srcObject) {
+            const tracks = videoRef.srcObject.getTracks()
+            tracks.forEach( track => track.stop())
+            videoRef.srcObject = null
+            isCapturing = false
+        }
+    }
+
+    const saveCapture = () => {
+        if(!videoRef || !canvasForSave) return
+        canvasForSave.width = videoSize.width
+        canvasForSave.height = videoSize.height
+        const ctx = canvasForSave.getContext('2d')
+        ctx.clearRect(0, 0, videoSize.width, videoSize.height)
+        ctx.drawImage(videoRef, 0, 0)
+        const data = canvasForSave.toDataURL("image/jpeg", 1)
+        download(data, "output.jpg", "image/jpeg")
+    }
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+    <div class='button-wrapper'>
+        {#if isCapturing}
+            <Button on:click={stopCapture} variant='raised'>
+                キャプチャーを終了
+            </Button>
+        {:else}
+            <Button on:click={startCapture} variant='raised'>
+                画面キャプチャーを開始する
+            </Button>
+        {/if}
+        <Button on:click={saveCapture} variant='raised'>
+            キャプチャーを保存
+        </Button>
+    </div>
+    <div class='capture-wrapper'>
+        {#if isCapturing}
+            <p class='attention'>この画面が保存されます。</p>
+        {/if}
+        <video bind:this={videoRef} autoplay class='capture'>
+            <track kind="captions" src=''>
+        </video>
+        <canvas bind:this={canvasForSave} class='hide'></canvas>
+    </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
+    .capture-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
-
-    p {
-      max-width: none;
+    .capture {
+        max-width: 80vw;
+        max-height: 80vh;
     }
-  }
+    .attention {
+        font-weight: bold;
+        margin-bottom: 16px;
+    }
+    .hide {
+        display: none;
+    }
 </style>
